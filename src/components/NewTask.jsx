0981f616a7f21fc0657b47dc8304442033c096d7
Khanhbroo <khanhmathias532211@gmail.com>
2025-05-04 10:26:12 +0700

    Clearing Tasks & Finishing The Project

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const NewTask = ({ onAdd }) => {
   const [enteredTask, setEnteredTask] = useState("");
+  const inputRef = useRef();
 
   const handleChange = (event) => {
     setEnteredTask(event.target.value);
   };
 
   const handleClick = () => {
+    if (enteredTask.trim().length === 0) {
+      return;
+    }
     onAdd(enteredTask);
     setEnteredTask("");
+    inputRef.current.focus();
   };
 
   return (
     <div className="flex items-center gap-4">
       <input
+        ref={inputRef}
         type="text"
         className="w-64 px-2 py-1 rounded-sm bg-stone-200"
         value={enteredTask}
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -15,7 +15,10 @@ const Task = ({ tasks, onAdd, onDelete }) => {
           {tasks.map((task) => (
             <li key={task.id} className="flex justify-between my-4">
               <span>{task.text}</span>
-              <button className="text-stone-700 hover:text-red-500 transition-colors">
+              <button
+                className="text-stone-700 hover:text-red-500 transition-colors"
+                onClick={() => onDelete(task.id)}
+              >
                 Clear
               </button>
             </li>
